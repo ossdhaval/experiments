63fefb3236d504e6e44837bc0962f1dc76e926c3
xpetra <dhavaltdesai@yahoo.com>
2014-02-10 14:52:24 +0530

    comparable example

---
+++ src/com/compare/Caller.java
@@ -0,0 +1,44 @@
+package com.compare;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class Caller 
+{
+	public static MyObject mo1 = null;
+	public static MyObject mo2 = null;
+	
+	public static void main(String[] args)
+	{
+		mo1 = new MyObject();
+		mo2 = new MyObject();
+		
+		mo1.setmAge(20);
+		mo2.setmAge(10);
+		
+		List<MyObject> ml = new ArrayList<MyObject>();
+		
+		ml.add(mo1);
+		ml.add(mo2);
+		
+		System.out.println("printing simple list ( it'll print insertion order ) : ");
+		for(MyObject o : ml)
+		{
+			System.out.println(o.getmAge());
+		}
+		
+		Collections.sort(ml);
+		System.out.println("printing natural order ( implemented by comparable ) : ");
+		for(MyObject o : ml)
+		{
+			System.out.println(o.getmAge());
+		}
+		
+		
+		
+	}
+}
--- src/com/compare/MyObject.java
+++ src/com/compare/MyObject.java
@@ -1,7 +1,31 @@
 package com.compare;
 
-public class MyObject 
+public class MyObject implements Comparable
 {
-	public int mAge = 0;
+	private int mAge = 0;
+
+	public int getmAge() {
+		return mAge;
+	}
+
+	public void setmAge(int mAge) {
+		this.mAge = mAge;
+	}
+
+	public int compareTo(Object o) {
+		// if this is less than arg then return -1
+		if(this.getmAge() <= ((MyObject)o).getmAge())
+			return -1;
+		// if this is less than arg then return -1
+		if(this.getmAge() == ((MyObject)o).getmAge())
+			return 0;
+		// if this is less than arg then return -1
+		if(this.getmAge() >= ((MyObject)o).getmAge())
+			return 1;
+		
+		return 2;
+	}
+	
+	
 	
 }
