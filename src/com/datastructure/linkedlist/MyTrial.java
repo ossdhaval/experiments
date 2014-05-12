e5ed719d4068107e7712c8b0677ec6400e368ef5
xpetra <dhavaltdesai@yahoo.com>
2014-05-12 12:09:59 +0530

    Balance array

---
+++ src/com/datastructure/linkedlist/MyTrial.java
@@ -0,0 +1,14 @@
+package com.datastructure.linkedlist;
+
+public class MyTrial {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+
+	}
+
+}
